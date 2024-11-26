3dcb2850e20ee4449a02e8fe60231f7ebcb1a3a8
h$.weird <dmanh20112000@gmail.com>
2024-11-26 10:09:59 +0700

    :sparkles: feat set up document api

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { useContainer } from 'class-validator';
 import { initializeTransactionalContext } from 'typeorm-transactional';
 import { ValidationPipe } from '@/shared/pipes/validate.pipe';
 import { HttpExceptionFilter } from '@/infra/filters/http-exception.filter';
+import { IAdapterSecret } from '@/infra/config/secret/adapter';
+import { RequestMethod } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   initializeTransactionalContext();
@@ -15,6 +18,23 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(3000);
+  app.setGlobalPrefix('api', {
+    exclude: [
+      { path: 'health', method: RequestMethod.GET },
+      { path: '/', method: RequestMethod.GET },
+    ],
+  });
+
+  const { APP_PORT, APP_NAME } = app.get(IAdapterSecret);
+
+  const config = new DocumentBuilder()
+    .setTitle(APP_NAME)
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
+  await app.listen(APP_PORT);
 }
 bootstrap();
